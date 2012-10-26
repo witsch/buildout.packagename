96706c537425fdc944ef1bc1d8a71d4f3a640722
Andreas Zeidler <az@zitc.de>
2012-10-27 00:31:35 +0200

    Use a separately created `setup.py` (and package) for testing.
    Testing against the buildout extension itself doesn't make sense as it's always importable due to being installed via `pip`.

--- buildout/packagename/tests.py
+++ buildout/packagename/tests.py
@@ -1,6 +1,8 @@
+from os import chdir
 from os.path import join
 from string import strip
-from subprocess import check_call
+from textwrap import dedent
+from subprocess import check_call, check_output
 from pkg_resources import get_distribution
 
 
@@ -14,13 +16,17 @@ def extract_buildout():
 
 
 def test_buildout_uses_package_name(tmpdir):
-    app = tmpdir.join('app')
     buildout = tmpdir.join('buildout.cfg')
     buildout.write(extract_buildout())
-    check_call([
-        join(home, 'bin', 'buildout'),
-        'buildout:directory=%s' % home,
-        'app:interpreter=%s' % app,
-        '-c', str(buildout)])
+    tmpdir.join('setup.py').write(dedent("""
+        from setuptools import setup
+        setup(name='foo', version='1.0', packages=['foo'])
+    """))
+    tmpdir.mkdir('foo').join('__init__.py').write(dedent("""
+        print 'hello!'
+    """))
+    chdir(str(tmpdir))
+    check_call([join(home, 'bin', 'buildout')])
+    app = tmpdir.join('bin', 'app')
     app.check(file=True)
-    check_call([str(app), '-c', 'import %s' % name])
+    assert check_output([str(app), '-c', 'import foo']) == 'hello!\n'
